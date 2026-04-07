890f118b62e800942d49d21d35b3ef6d2a1300d8
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2026-04-07 20:46:33 -0300

    feat: cria packages entity, repository, service e controller. E dentro de cada um cria suas respectivas classes para 'Category'

---
+++ src/main/java/com/movieflix/entity/Category.java
@@ -0,0 +1,20 @@
+package com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column()
+    private String name;
+
+
+
+}

8a8586c86ed0bd16777a1b91655d4fb8370370bf
Felipe de Oliveira Galbiati <gaalbiatii.felipe@gmail.com>
2026-04-07 21:02:41 -0300

    feat: Injeta dependencias em CategoryService/Controller, e cria endpoint findAllCategories que utiliza metodo findAll do SQL atraves da JPA

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -1,9 +1,29 @@
 package com.movieflix.controller;
 
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    /*
+    * injeção de dependencia
+    * -Com anotação @Autowired
+    * -Usando Lombok, com anotação @RequiredArgsConstructor
+    * -Criando um construtor para o atributo
+    * */
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> findAllCategories() {
+        return categoryService.findAll();
+    }
 }
--- src/main/java/com/movieflix/service/CategoryService.java
+++ src/main/java/com/movieflix/service/CategoryService.java
@@ -1,7 +1,26 @@
 package com.movieflix.service;
 
+import com.movieflix.entity.Category;
+import com.movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    /*
+     * injeção de dependencia
+     * -Com anotação @Autowired
+     * -Usando Lombok, com anotação @RequiredArgsConstructor
+     * -Criando um construtor para o atributo
+     * */
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll(){
+        return categoryRepository.findAll();
+    }
+
 }
